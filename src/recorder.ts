f64ca2b0e34450ddb2b56abf46212758c2d1363e
lisnote <didongxiaoli@gmail.com>
2022-11-09 16:12:56 +0800

    将代理获取的数据转化为mock

--- src/recorder.ts
+++ src/recorder.ts
@@ -18,32 +18,24 @@ export default function (target: string) {
     })
       .catch((e) => e.response)
       .then((proxyRes) => {
+        // 代理请求
         res.status(proxyRes.status);
         Object.entries(proxyRes.headers).forEach(([key, value]) => {
           res.setHeader(key, value as string);
         });
         res.setHeader("access-control-allow-origin", "*");
         res.send(proxyRes.data);
+        // 创建 mocker 记录
+        if (proxyRes.status != "200") return;
+        const dir = join(__dirname, "data/recorder");
+        const file = req.path.replaceAll("/", "_") + ".json";
+        const jsonData: Record<string, any> = {};
+        jsonData[req.method] = {};
+        jsonData[req.method][req.path] = proxyRes.data;
+        mkdir(dir, { recursive: true }).then(() => {
+          writeFile(join(dir, file), JSON.stringify(jsonData, null, 2), "utf8");
+        });
       })
       .catch((e) => res.send(e));
   };
 }
-
-// export default createProxyMiddleware({
-//   target: 'https://api.github.com',
-//   changeOrigin: true,
-//   selfHandleResponse: true,
-//   onProxyRes: responseInterceptor(async (respBuffer, proxyRes, req) => {
-//     const pathName = req.url.replace(/\?.*/, '');
-//     const data = respBuffer.toString('utf8');
-//     const dir = join(__dirname, 'data/recorder');
-//     const file = req.url.replace(/\?.*/, '').replaceAll('/', '_') + '.json';
-//     const jsonData = {};
-//     jsonData[req.method] = {};
-//     jsonData[req.method][pathName] = JSON.parse(data);
-//     mkdir(dir, { recursive: true }).then(() => {
-//       writeFile(join(dir, file), JSON.stringify(jsonData, null, 2), 'utf8');
-//     });
-//     return respBuffer;
-//   }),
-// });
