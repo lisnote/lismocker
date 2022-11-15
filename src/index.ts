be467476ac3695ff32af3d687217b1e849f65133
lisnote <didongxiaoli@gmail.com>
2022-11-15 14:58:57 +0800

    增加重启服务功能

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
 import { config } from "dotenv-flow";
 import express from "express";
+import destroyer from "./destroyer";
 import cors from "./cors";
 import options from "./options";
 import update from "./update";
 import mocker from "./mocker";
 import recorder from "./recorder";
 
+import type { Server } from "http";
+
 config();
 const { PORT, PROXY_BACKEND } = process.env as Record<string, string>;
 
+let server: Server;
 function startServer(port: string, proxy: string) {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(destroyer);
   app.use(cors);
   app.options("*", options);
   app.use(update);
   app.use(mocker());
   app.use(recorder(proxy));
-  app.listen(port, () =>
+  server = app.listen(port, () =>
     console.log(`mocker started in \x1b[1;36mhttp://localhost:${port}\x1b[0m`)
   );
 }
 
-export default startServer;
+export { startServer as default, server };
 
 startServer(PORT, PROXY_BACKEND);
--- src/update.ts
+++ src/update.ts
@@ -1,10 +1,22 @@
 import type { Request, Response, NextFunction } from "express";
 import { initMocker } from "./mocker";
 import { initRecorder } from "./recorder";
+import startServer, { server } from "./index";
+import { config } from "dotenv-flow";
 
-const pathMapper: Record<string, Function> = {
-  mocker: initMocker,
-  recorder: initRecorder,
+const pathMapper: Record<string, (req: Request, res: Response) => void> = {
+  init(req: Request, res: Response) {
+    server.close();
+    res.destroy();
+    const env = config({ silent: true }).parsed as Record<string, string>;
+    Object.entries(env).forEach(([key, value]) => {
+      process.env[key] = value;
+    });
+    const { PORT, PROXY_BACKEND } = env;
+    startServer(PORT, PROXY_BACKEND);
+  },
+  mocker: () => initMocker(),
+  recorder: () => initRecorder(),
 };
 
 export default function (req: Request, res: Response, next: NextFunction) {
@@ -24,8 +36,8 @@ function updateProxy(
 ) {
   if (path === "/update/" + key) {
     console.log("update access", req.method, req.path);
-    fn();
     res.end("update: " + key);
+    fn(req, res);
     return true;
   }
 }
