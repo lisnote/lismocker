d80e7e02b5d4c7e0f73503d8e97a19eb8517bf0a
lisnote <lisnote@lisnote.com>
2024-04-09 15:01:11 +0800

    fix: 请求头错误

--- src/cors.ts
+++ src/cors.ts
@@ -1,7 +1,7 @@
 import type { Request, Response, NextFunction } from 'express';
 export default function cors(req: Request, res: Response, next: NextFunction) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Expose-Headers', '*');
   next();
